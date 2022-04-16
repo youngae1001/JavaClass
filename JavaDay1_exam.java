ba1ef7def70f1f5fd4a312ca509e8ef6a15269cd
youngae1001 <103499788+youngae1001@users.noreply.github.com>
2022-04-16 14:44:43 +0900

    Add files via upload

--- JavaDay1_exam.java
+++ JavaDay1_exam.java
@@ -51,32 +51,32 @@ public class JavaDay1_exam {
 		// ============ 문제3 ============
 	    // 문제 : a와 b가 가지고 있는 값을 서로 뒤바꿔주세요. // 조건 : 숫자와 사칙연산 사용 금지
 	    
-//	    Random ran = new Random();
-//
-//	    // `int a` => a라는 이름의 변수(즉 공간)을 만든다.
-//	    // `int` => a라는 공간에는 정수만 담을 수 있다.
-//	    // `;` => 문장이 끝남을 알린다.
-//	    int a;
-//	    a = ran.nextInt(100);
-//	    int b = ran.nextInt(100);
-//	    
-//	    // `+` => 문장과 문자을 합친다.
-//	    System.out.println("a : " + a);
-//	    System.out.println("b : " + b);
-//	    
-//	    // 여기서 부터 
-//
-//	    int c = a;
-//	    a = b;
-//	    b = c;
-//
-//
-//	    // 여기까지 수정 가능
-//	    
-//	    System.out.println("a : " + a);
-//	    // 출력 : a : b의 값
-//	    System.out.println("b : " + b);
-//	    // 출력 :  b : a의 값
+	    Random ran = new Random();
+
+	    // `int a` => a라는 이름의 변수(즉 공간)을 만든다.
+	    // `int` => a라는 공간에는 정수만 담을 수 있다.
+	    // `;` => 문장이 끝남을 알린다.
+	    int a;
+	    a = ran.nextInt(100);
+	    int b = ran.nextInt(100);
+	    
+	    // `+` => 문장과 문자을 합친다.
+	    System.out.println("a : " + a);
+	    System.out.println("b : " + b);
+	    
+	    // 여기서 부터 
+
+	    int c = a;
+	    a = b;
+	    b = c;
+
+
+	    // 여기까지 수정 가능
+	    
+	    System.out.println("a : " + a);
+	    // 출력 : a : b의 값
+	    System.out.println("b : " + b);
+	    // 출력 :  b : a의 값
 	}
     
 }
